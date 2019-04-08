60a55ebfa081d81906178ecba1a721f5253a59bd
Ernad Delic <ernad.delic77@gmail.com>
2019-04-08 19:49:45 +0200

    Updated code, now it uses indexOf instead of having two separate arrays

--- Resistor-color/script.js
+++ Resistor-color/script.js
@@ -1,12 +1,3 @@
-const colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const colorCode = (input) => {
-  let result = 0;
-  for (let i = 0; i < colors.length; i++) {
-    if (colors[i] === input) {
-      result += numbers[i];
-    }
-  }
-  return result;
-};
-console.log(colorCode("orange"));
+const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+const colorCode = color =>  COLORS.indexOf(color);       
+console.log(colorCode("black")); 
