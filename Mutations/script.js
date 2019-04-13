cea3b8ae722e5d7b53f6ef6dfcf98cad0396045d
Ernad Delic <ernad.delic77@gmail.com>
2019-04-13 15:48:52 +0200

    Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array

---
+++ Mutations/script.js
@@ -0,0 +1,12 @@
+const mutation = (arr) => {
+  let arr0 = arr[0].toLowerCase();
+  let arr1 = arr[1].toLowerCase();
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr0.indexOf(arr1[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+};
+let result = mutation(["Hello", "hey"]);
+console.log(result);
